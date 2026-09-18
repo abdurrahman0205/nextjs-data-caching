48a43b0b10ad1e86d91e0c5d4cbe6910f31d414e
Abdur Rahman <dev.abdurrahman0205@gmail.com>
2026-09-18 17:53:38 +0600

    Tried and Learned about various formate of fetching data, throw new error,res.ok, try,catch,finally etc

---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostLoading = () => {
+  return (
+    <div className='mx-auto container items-center'>
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+};
+
+export default PostLoading;
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
 
-const PostPage = () => {
+//Another method of data load/fetch in SSC
+// const postPromise = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+//   return res.json();
+// }
+
+/**
+ * GET:
+ * POST:
+ * UPDATE: put, patch
+ * DELETE:
+ * 
+*/
+
+//Basic formate/system
+
+const getPost = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  return res.json();
+}
+
+//!res.ok, throw new Error
+// const getPost = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch posts")
+//   }
+
+//   return res.json();
+// }
+
+
+//try,catch,finally
+// const getPost = async () => {
+//   try {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+//     return res.json();
+//   } catch(error) {
+//         throw new Error("Failed to fetch posts")
+//   } finally {
+//         console.log("Finished.");
+//   }
+// }
+
+
+const PostPage = async () => {
+
+  // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // const posts = await res.json();
+
+  const posts = await getPost();
+
   return (
     <div>
-      <h1>Hello! Post here</h1>
+      <h1>Total posts: {posts.length}</h1>
     </div>
   );
 };
